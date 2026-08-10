f3c9809770b89e662d69f57b15ce750e43c19611
leno-mpm <s.pazmino16@gmail.com>
2026-08-10 15:49:57 -0500

    Implementa endpoints del modulo de comunidades

--- app/Models/Comunidad.php
+++ app/Models/Comunidad.php
@@ -13,6 +13,11 @@ class Comunidad extends Model
         'logo_url', 'correo_contacto', 'instagram', 'fecha_fundacion', 'activa',
     ];
 
+    protected $casts = [
+        'activa'          => 'boolean',
+        'fecha_fundacion' => 'date:Y-m-d',
+    ];
+
     public function membresias()
     {
         return $this->hasMany(Membresia::class);
